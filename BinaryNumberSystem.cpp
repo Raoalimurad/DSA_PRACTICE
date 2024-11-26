ff98521d57a7e5b043c842f5af2ba366ccfabddc
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-11-26 13:23:21 +0500

    Add files via upload

---
+++ BinaryNumberSystem.cpp
@@ -0,0 +1,47 @@
+
+// #include <iostream>
+// using namespace std;
+// int decimalToBinary(int num){
+//     int ans = 0, power = 1;
+//     while(num>0){
+//        int rem = num%2; /// for last digit
+//         num/=2;
+//         ans+=(rem*power);
+//         power= power*10;
+//     }
+//     return ans;
+// }
+
+// int binToDecimal(int num){
+//     int ans = 0,pow = 1;
+//     while(num>0){
+//         int rem = num%10;
+//         num/=10;
+//         ans+=(rem*pow);
+//         pow*=2;
+//     }
+//     return ans;
+// }
+
+
+// int main() {
+
+// 
+//     int num = 10;
+//  int binnum = 1100101;
+//   cout<<"decimal to binary is "<<decimalToBinary(num)<< endl;
+//  cout<<"binary to decimal is "<<binToDecimal(binnum);
+
+
+
+
+
+
+
+
+
+
+
+
+//     return 0;
+// }
