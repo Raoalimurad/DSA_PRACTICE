434bea83d451a32fb0af1d4ac6d9d7bcfbcad2c2
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2025-01-07 13:33:04 +0500

    Update sort.cpp

--- STL/Algorithm/sort.cpp
+++ STL/Algorithm/sort.cpp
@@ -9,7 +9,10 @@ int main() {
    for(int i = 0; i < n; i++){
        cout<<arr[i]<<" ";
    }
-   
+   ////sort in descending order
+
+   // greater<int> is camparater which allow sorting in descending order
+   sort(arr,arr+n,greater<int>())
 
     return 0;
 }
