254ecb64a5b401761af2b2a5c1ba8d96b1ede7c9
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2025-01-29 09:37:43 +0500

    Add files via upload

---
+++ 2DVector.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+int main() {
+ vector<vector <int> > mat = {{1,2,3},{24,26,28,30}};
+ ///rows = mat.size();
+ //col = mat[i].size()
+ 
+ for(int i = 0 ; i < mat.size();i++){
+     for(int j = 0 ; j < mat[i].size();j++){
+         cout<<mat[i][j]<<" ";
+     }
+     cout<<endl;
+ }
+
+ 
+    return 0;
+}
