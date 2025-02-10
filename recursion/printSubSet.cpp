3872a029cef4dc24e73b400b5ef5a3ccafa35a4e
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2025-02-10 14:35:24 +0500

    Add files via upload

---
+++ recursion/printSubSet.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include<vector>
+using namespace std;
+void printSubSet(vector<int>&arr,vector<int>& ans,int i){
+    if(i == arr.size()){
+       for(int val:ans){
+           cout<<val<<" ";
+       }
+        cout<<endl;
+        return;
+    }
+    ans.push_back(arr[i]);
+    printSubSet(arr,ans,i+1);
+    ans.pop_back();///backtracking
+      printSubSet(arr,ans,i+1);
+  
+}
+int main() {
+ vector<int> arr = {1,2,3};
+   vector<int>ans;
+  
+  printSubSet(arr,ans,0);
+ 
+    return 0;
+}
